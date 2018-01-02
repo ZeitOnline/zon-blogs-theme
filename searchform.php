4a8e87cacfbddcfa70c11872aa8f676e27e7d254
Nico Bruenjes <nico.bruenjes@gmail.com>
2018-01-02 17:47:45 +0100

    Remove links to old zb directory and version bump

--- searchform.php
+++ searchform.php
@@ -3,7 +3,7 @@
 	<input class="search__input" id="s" name="s" type="search" placeholder="Suche">
 	<button class="search__button" type="submit">
 		<svg class="search__icon" role="img" aria-labelledby="title">
-        <use xlink:href="<?php echo site_url() ?>/wp-content/themes/zb/images/icons/icons.svg?312e36312e322e64657630#svg-search"></use>
+        <use xlink:href="<?php echo get_stylesheet_directory_uri(); ?>/images/icons/icons.svg?312e36312e322e64657630#svg-search"></use>
     </svg>
 	</button>
 </form>
