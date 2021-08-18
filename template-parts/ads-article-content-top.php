707f7721304e950c416a688fdbfef1798479dfdf
Nico Bruenjes <nico.bruenjes@gmail.com>
2021-08-18 11:33:31 +0200

    ZON-6781: Use zb_place_ad on article body

--- template-parts/ads-article-content-top.php
+++ template-parts/ads-article-content-top.php
@@ -6,7 +6,7 @@
  */
 
 if (! zb_get_meta( 'zb_hide_medium_rectangle' ) && ( zb_get_meta( 'zb_medium_rectangle' ) != 'hide-medium-rectangle' ) ) {
-	zb_render_ad( 'desktop', '8', 'ad-wrapper ad-wrapper--article ad-container', 'article', 'Anzeige' );
-	zb_render_ad( 'mobile', '3', 'ad-wrapper ad-wrapper--article ad-container' );
+	zb_place_ad( 3, 'mobile' );
+	zb_place_ad( 8, 'desktop', ['ad-floated-right'] );
 }
 ?>
